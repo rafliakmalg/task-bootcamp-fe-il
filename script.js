49f96fc47d20f6be8fdb02a86f82615913fa2c35
rafliakmalg <apaajapcture@gmail.com>
2024-03-22 16:11:07 +0700

    Done Bang

---
+++ script.js
@@ -0,0 +1,9 @@
+let text = document.querySelector(".text");
+
+var typed = new Typed(text, {
+    strings: ["Full Stack Developer", "Front End Developer", "Back End Developer"],
+    typeSpeed: 50,
+    backSpeed: 30,
+    backDelay: 1000,
+    loop: true,
+});
